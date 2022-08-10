acf41923dcb56b9410a543f41a234dfe067015e4
Tejeswarreddy <109336274+Tejeswarreddy-3i@users.noreply.github.com>
2022-08-10 14:32:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,4 @@ setup(
     name='helloworld',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ],
 )
